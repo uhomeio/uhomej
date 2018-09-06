718e5082562077ae73f11d17e1df08c020b0216e
majingfeng <mahda@163.com>
2018-09-06 15:14:12 +0800

    add todo task for whether or not start the fork task

--- uhomej-core/src/main/java/org/ethereum/config/net/ETCNetConfig.java
+++ uhomej-core/src/main/java/org/ethereum/config/net/ETCNetConfig.java
@@ -24,7 +24,7 @@ import org.ethereum.config.blockchain.*;
  */
 public class ETCNetConfig extends BaseNetConfig {
     public static final ETCNetConfig INSTANCE = new ETCNetConfig();
-
+// todo 可以在这里修改是否启用分叉
     public ETCNetConfig() {
         add(0, new FrontierConfig());
         add(1_150_000, new HomesteadConfig());
